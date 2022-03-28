2f417fd996f4f1fd0f4514acdad06fa44ea2d845
rusty619 <rusty6916@gmail.com>
2022-03-28 16:07:44 -0400

    commit tree 8e822fdab409d3fba78a75189872722672006d76

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+     .then(res => {
+       return res.json();
+     })
+     .then((data) => {
+       return renderBooks(data)
+     })
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerText = book.name;
     main.appendChild(h2);
   });
 }
